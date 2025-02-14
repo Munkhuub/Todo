f3155f6224a16bc2a7b1ecd55fd40d088e34eacc
24LP9763 24LP9763 <24LP9763@administrators-iMac-3.local>
2025-02-14 18:04:27 +0800

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,71 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+
+  let list = [];
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+  const handleAdd = (event) => {
+    console.log(input);
+  };
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.todo}>
+        <h1>To-Do list</h1>
+        <div className={styles.inputContainer}>
+          <input type="text" value={input} onChange={handleInputChange} />
+          <button type="submit" onClick={handleAdd}>
+            Add
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.buttons}>
+          <button className={styles.all}>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+        <p>No tasks yet. Add one above!</p>
+      </div>
+
+      <div className={styles.poweredText}>
+        <p>Powered by</p>
+        <p className={styles.blue}>Pinecone academy</p>
+      </div>
     </div>
   );
 }
+// arr.map yvna, shine array ruu pushleh
+// export default function Home() {
+//   const [count, setCount] = useState(0);
+//   const [text, setText] = useState("");
+//   const handleInc = () => {
+//     setCount(count + 1);
+//   };
+//   const handleMinus = () => {
+//     setCount(count - 1);
+//   };
+//   const handleInputChange = (event) => {
+//     setText(event.target.value);
+//   };
+
+//   return (
+//     <div className={styles.page} style={{ margin: "auto" }}>
+//       <div>
+//         <button onClick={handleInc}>Plus</button>
+
+//         <button onClick={handleMinus}>Minus</button>
+
+//         <p>Count: {count}</p>
+//       </div>
+//       <div>
+//         <input type="text" onChange={handleInputChange} value={text}></input>
+//         <div>{text.length}</div>
+//       </div>
+//     </div>
+//   );
+// }
